f15c30640fa81352b17da4432a2a3fa2ba982dba
nadzimalrash <hello@nadzim.com>
2023-10-15 15:47:34 +0800

    Done repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    myString = "";
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            myString = myString + string;
+        }
+        return myString;
+    }
 };
 
 // Do not edit below this line
