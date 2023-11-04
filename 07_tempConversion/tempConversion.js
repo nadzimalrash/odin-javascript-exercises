9b1978561f0ce9c0d839636b3b800138093e22f8
nadzimalrash <hello@nadzim.com>
2023-11-04 14:24:18 +0800

    complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  const degC = (degF - 32) * 5/9;
+  return Math.round(degC * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  const degF = (degC * 9/5) + 32;
+  return Math.round(degF * 10)/10;
 };
 
 // Do not edit below this line
