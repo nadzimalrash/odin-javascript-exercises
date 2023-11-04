2cf6e26255ffeff3f4395c896efe8b9512679baa
nadzimalrash <hello@nadzim.com>
2023-11-04 13:03:59 +0800

    complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,17 @@
-// const removeFromArray = function(array, item) {
-//     const inputArray = [array];
-//     console.table(inputArray);
-//     console.log(inputArray.splice(2, 1));
-// };
+const removeFromArray = function(array, ...items) {
+    console.log(items.length);
+    for (let i = 0; i < items.length; i++) {
+        checkItem = array.indexOf(items[i]);
+        console.log(checkItem);
+        if (checkItem >= 0) {
+            const index = array.indexOf(items[i]);
+            const removal = array.splice(index, 1);
+        } else {
+            console.log(items[i], "does not exist");
+        }
+    }
+    return array;
+}
 
-// // Do not edit below this line
-// // module.exports = removeFromArray;
-
-const inputArray = ["a", "b", "c", "d"];
-console.log(inputArray);
+// Do not edit below this line
+module.exports = removeFromArray;
