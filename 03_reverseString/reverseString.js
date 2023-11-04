f0df25eb77eb739725a3c326cc79015756a27d03
nadzimalrash <hello@nadzim.com>
2023-11-04 11:56:37 +0800

    complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // const stringLength = string.length;
+    if (string.length > 0) {
+        const stringArray = Array.from(string);
+        let reversed = [];
+        for (let i = 1; i <= string.length; i++) {
+            reversed += stringArray[string.length - i];
+        }
+        return reversed;
+    } else {
+        return '';
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+// const removeFromArray = function(array, item) {
+//     const inputArray = [array];
+//     console.table(inputArray);
+//     console.log(inputArray.splice(2, 1));
+// };
 
-};
+// // Do not edit below this line
+// // module.exports = removeFromArray;
 
-// Do not edit below this line
-module.exports = removeFromArray;
+const inputArray = ["a", "b", "c", "d"];
+console.log(inputArray);
