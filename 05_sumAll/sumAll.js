78bf18d364b36abcef30f0828873346599cad037
nadzimalrash <hello@nadzim.com>
2023-11-04 13:31:18 +0800

    complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a >= 0 && b >= 0 && typeof a === 'number' && typeof b === 'number') {
+        const first = Math.min(a,b);
+        const last = Math.max(a,b);
+        let total = 0;
+        for (let i = first; i <= last; i++) {
+            total = total + i;
+        }
+        return total;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
