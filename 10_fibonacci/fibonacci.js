0a3616c9c1b9a568b5b4de13da6b06ecf0bb2188
nadzimalrash <hello@nadzim.com>
2023-12-25 16:56:34 +0800

    complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    Number(nth);
+    if (nth < 0) {
+        return "OOPS";
+    }
+    else if (nth == 1) {
+        return 1;
+    }
+    else {
+        let n1 = 0;
+        let n2 = 1;
+        let next = 0;
+        for (i = 1; i < nth; i++) {
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+        console.log(next);
+        return next;
+    }
 
 };
 
