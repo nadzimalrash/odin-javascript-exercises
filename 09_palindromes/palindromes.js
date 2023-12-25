705e1855fee075d756aea084d6d5c48755109cb8
nadzimalrash <hello@nadzim.com>
2023-12-25 16:16:07 +0800

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const oldString = string.toLowerCase().replace(/[^a-z0-9]/gi, '');
+    const newString = oldString.split('').reverse().join('');
+    return (newString == oldString);
 };
+
+// palindromes("race_____ )*&&)&)car");
 
 // Do not edit below this line
 module.exports = palindromes;
