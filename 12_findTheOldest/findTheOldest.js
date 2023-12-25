f786ff5129d08f58f91cd9cfaceda39ba96a9801
nadzimalrash <hello@nadzim.com>
2023-12-25 18:47:01 +0800

    complete findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    array.forEach(person => {
+        person.age = calcAge(person.yearOfDeath, person.yearOfBirth);
+    })
+    return array.reduce((accumulator, current) => {
+        return accumulator.age > current.age ? accumulator : current;
+    });
+}
 
-};
+const calcAge = function (death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
